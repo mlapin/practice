77aa5fb0e455256038b31d8655795d0fc3cd596b
Maksim Lapin <mlapin@mpi-inf.mpg.de>
2016-10-06 08:44:01 +0000

    maximum subarray

--- maximum_subarray/main.cpp
+++ maximum_subarray/main.cpp
@@ -8,6 +8,17 @@ using namespace std;
 
 int maxSubArray(vector<int>& a) {
     if (a.empty()) return 0;
+    int ms = a[0], n = a.size();
+    for (int i = 1, s = 0; i < n; ++i) {
+        s += a[i];
+        ms = max(ms, s);
+        s = s > 0 ? s : 0;
+    }
+    return ms;
+}
+
+int maxSubArray1(vector<int>& a) {
+    if (a.empty()) return 0;
     int ms = a[0], i = 1, n = a.size();
     while (ms <= 0 && i < n) {
         ms = max(ms, a[i++]);
@@ -30,5 +41,11 @@ int main() {
     cpu_duration = (clock() - startcputime) / (double) CLOCKS_PER_SEC;
     cout << "ms: " << cpu_duration*1000.0 << endl;
 
-    cout << "max area: " << r << endl;
+    startcputime = clock();
+    auto r1 = maxSubArray1(a);
+    cpu_duration = (clock() - startcputime) / (double) CLOCKS_PER_SEC;
+    cout << "ms: " << cpu_duration*1000.0 << endl;
+
+    cout << "max sum: " << r << endl;
+    cout << "max sum: " << r1 << endl;
 }
